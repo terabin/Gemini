f7d5447566471967fa4f6a04a0cc683eb03f2270
Revam <mikeystor@live.com>
2015-09-13 16:46:56 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -20,9 +20,9 @@ echo %appveyor_build_version% > build\%configuration%\VersionInfo.dat
 echo Packing files...
 7z a build\%configuration%\Gemini.zip %APPVEYOR_BUILD_FOLDER%\src\bin\*.exe
 
-echo Adding built files to Repo
-git config --global credential.helper store
-powershell add-content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
-git add build
+echo Adding built files to Git
+git config credential.helper store
+rem "https://$($env:access_token):x-oauth-basic@github.com" > "%USERPROFILE%\.git-credentials"
+git add -f build
 git commit -m "Add latest build from AppVeyor"
 git push %APPVEYOR_REPO_BRANCH%
